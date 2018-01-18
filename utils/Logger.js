c34735bc7504ccf28289664df4b7e251105b3d0a
Paradise Kelechi <paradisekelechi@gmail.com>
2018-01-18 22:05:12 +0100

    task(logging): implement application logging using winston

---
+++ utils/Logger.js
@@ -0,0 +1,45 @@
+/**
+ *  @fileOverview Helper function that sets up logger
+ *
+ *  @author Paradise Kelechi
+ *
+ * @requires NPM:winston
+ * @requires NPM:path
+ */
+import winston, { format } from "winston";
+import path from "path";
+
+const { combine, timestamp, label, prettyPrint } = format;
+
+const config = winston.createLogger({
+  format: combine(
+    label({
+      label: "Hellobooks"
+    }),
+    timestamp(),
+    prettyPrint()
+  ),
+  transports: [
+    new winston.transports.File({
+      filename: path.join(__dirname, "../logs/error.logs"),
+      level: "error"
+    }),
+    new winston.transports.File({
+      filename: path.join(__dirname, "../logs/combined.logs")
+    })
+  ]
+});
+
+const logger = (level, message) => {
+  config.log({
+    level,
+    message
+  });
+  return {
+    isLogged: true,
+    message,
+    level
+  };
+};
+
+export default logger;
