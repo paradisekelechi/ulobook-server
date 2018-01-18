0360933b9c434f735a21b0daee8729e4085cad10
Paradise Kelechi <paradisekelechi@gmail.com>
2018-01-18 21:27:45 +0100

    task(database): add database configuration for different environments

---
+++ utils/dbconfig.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default {
+  development: {
+    username: process.env.DB_USERNAME_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    database: "school",
+    host: "127.0.0.1",
+    port: 5432,
+    dialect: "postgres"
+  },
+  test: {
+    username: process.env.DB_USERNAME_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: "school",
+    host: "127.0.0.1",
+    port: 5432,
+    dialect: "postgres"
+  },
+  production: {
+    username: process.env.DB_USERNAME_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: "school",
+    host: "127.0.0.1",
+    port: 5432,
+    dialect: "postgres"
+  }
+};
